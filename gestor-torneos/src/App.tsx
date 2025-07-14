4b7492ef03f7f298065180f3482aebdb9c58ec31
francisco gil <franciscomarcelogil@gmail.com>
2025-07-14 18:25:59 -0300

    app add layout

--- gestor-torneos/src/App.tsx
+++ gestor-torneos/src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css'
+import './App.css';
+import Layout from './components/layout/layout.tsx';
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <Layout>
+        {' '}
+        <h1>hola</h1>
+      </Layout>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ gestor-torneos/src/components/layout/layout.tsx
@@ -0,0 +1,21 @@
+import type { ReactNode } from 'react';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>torneos</li>
+          <li>tablon </li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <main>{children}</main>
+      <footer> Diseñado por </footer>
+    </>
+  );
+}
