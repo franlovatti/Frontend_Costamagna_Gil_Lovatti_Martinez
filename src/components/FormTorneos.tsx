b9cadff2288787eb701eda105249b1711df30013
RicardLCost <costamagnarl@gmail.com>
2025-10-18 23:11:03 -0300

    Required agregado en formTorneo

--- src/components/FormTorneos.tsx
+++ src/components/FormTorneos.tsx
@@ -260,6 +260,7 @@ export default function FormTorneos() {
               name="localidad"
               value={form.localidad}
               onChange={handleChange}
+              required
             >
               <option value="">Seleccione la localidad</option>
               {dataLocalidades.map((localidad) => (
