405518558ce395c764533eb051a08ced075d3096
RicardLCost <costamagnarl@gmail.com>
2026-02-20 03:38:10 -0300

    nuevo hook usePariticapciones

--- src/hooks/useParticipaciones.ts
+++ src/hooks/useParticipaciones.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback} from 'react';
 import apiAxios from '../helpers/api';
 import type { Participation } from '../types';
 
@@ -28,6 +28,40 @@ export function useParticipaciones(partidoId: string, equipoId: string) {
       setLoading(false);
     }
   }, [partidoId, equipoId]);
+
+  useEffect(() => {
+    fetchParticipaciones();
+  }, [fetchParticipaciones]);
+
+  return { participaciones, loading, error };
+}
+
+export function useParticipacionesPorUsuarioEnTorneo(usuarioId: string, eventoId: string) {
+  const [participaciones, setParticipaciones] = useState<Participation[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchParticipaciones = useCallback(async () => {
+    if (!usuarioId || !eventoId) return;
+
+    setLoading(true);
+    setError(null);
+
+    try{
+      const response = await apiAxios.get(
+        '/participaciones/participacionesPorUsuarioEnTorneo',
+        {
+          params: { usuarioId, eventoId: eventoId },
+        }
+      );
+      setParticipaciones(response.data.data);
+    } catch (err) {
+      console.error('Error fetching participations:', err);
+      setError('Failed to fetch participations.');
+    } finally {
+      setLoading(false);
+    }
+  }, [usuarioId, eventoId]);
 
   useEffect(() => {
     fetchParticipaciones();
